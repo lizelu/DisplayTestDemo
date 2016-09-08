dc9442dd66a8d27d1752f451537cde8a9efcb136
zeluliCredit <zeluli@creditease.cn>
2016-09-08 17:48:00 +0800

    commit source code

---
+++ DisplayLayout/DisplayLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DisplayLayout
+//
+//  Created by Mr.LuDashi on 16/9/8.
+//  Copyright © 2016年 ZeluLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
